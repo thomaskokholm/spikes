be9dd6c814b2bb8cc061396c11258128253c889c
Bo Lorentsen <bl@lue.dk>
2015-10-30 19:37:26 +0100

    add FFL found on stackdump

--- cpp/jsonrpc/handler.cpp
+++ cpp/jsonrpc/handler.cpp
@@ -22,6 +22,27 @@
 
 using namespace std;
 
+// http://stackoverflow.com/questions/13358672/how-to-convert-a-lambda-to-an-stdfunction-using-templates
+// FFL convert lambda to a std::function
+template<typename T>
+struct memfun_type
+{
+    using type = void;
+};
+
+template<typename Ret, typename Class, typename... Args>
+struct memfun_type<Ret(Class::*)(Args...) const>
+{
+    using type = std::function<Ret(Args...)>;
+};
+
+template<typename F>
+typename memfun_type<decltype(&F::operator())>::type
+FFL(F const &func)
+{ // Function from lambda !
+    return func;
+}
+
 // http://stackoverflow.com/questions/26902633/how-to-iterate-over-a-tuple-in-c-11
 template<class F, class...Ts, std::size_t...Is>
 void for_each_in_tuple(const std::tuple<Ts...> & tuple, F func, std::index_sequence<Is...>){
