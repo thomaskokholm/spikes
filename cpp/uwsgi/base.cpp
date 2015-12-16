691341a0f8acad586d490e9830807a8b98865fb9
Bo Lorentsen <bl@lue.dk>
2015-12-16 23:46:23 +0100

    make simple uwsgi server build and run

--- cpp/uwsgi/base.cpp
+++ cpp/uwsgi/base.cpp
@@ -1,6 +1,7 @@
 #include "base.hpp"
 #include <uwsgi.h>
 #include <string>
+#include <iostream>
 
 using namespace std;
 using namespace uwsgi;
@@ -31,40 +32,57 @@ void Request::add_content_type( const string &mime_type ) {
 }
 
 void Request::write_body( const string &body ) {
-   uwsgi_response_write_body_do(_wsgi_req, (char *)body.data(), body.length());
+    uwsgi_response_write_body_do(_wsgi_req, (char *)body.data(), body.length());
 }
 
 // clog wrapper would also be cool !!!
-void log( const string &s )
+void uwsgi::log( const string &s )
 {
     uwsgi_log((s + "\n").c_str() );
 }
 
 // Add a handler function for all incoming requests, mutex ?
 uwsgi::reqfn_t req_handler;
-void register_request( reqfn_t fn ) {
+void uwsgi::register_request( reqfn_t fn ) {
     req_handler = fn;
 }
 
-extern "C" int uwsgi_cplusplus_init(){
-    uwsgi::log("Initializing example c++ plugin");
-    return 0;
-}
-
-extern "C" int uwsgi_cplusplus_request(wsgi_request *wsgi_req) {
-     // get uwsgi variables
-    if (uwsgi_parse_vars(wsgi_req)) {
-        uwsgi::log("Invalid request. skip.");
-    } else {
-        Request req( wsgi_req );
-
-        if( req_handler )
-            req_handler( req );
+extern "C" {
+    int uwsgi_cplusplus_init(){
+        uwsgi::log("Initializing example c++ plugin");
+        return 0;
     }
-    return UWSGI_OK;
-}
 
-extern "C" void uwsgi_cplusplus_after_request(wsgi_request *wsgi_req) {
-    // call log_request(wsgi_req) if you want a standard logline
-    uwsgi::log("logging c++ request");
+    int uwsgi_cplusplus_request(wsgi_request *wsgi_req) {
+        log( __PRETTY_FUNCTION__ );
+         // get uwsgi variables
+        if (uwsgi_parse_vars(wsgi_req)) {
+            uwsgi::log("Invalid request. skip.");
+        } else {
+            Request req( wsgi_req );
+
+            if( req_handler )
+                req_handler( req );
+        }
+        return UWSGI_OK;
+    }
+
+    void uwsgi_cplusplus_after_request(wsgi_request *wsgi_req) {
+        // call log_request(wsgi_req) if you want a standard logline
+        uwsgi::log("logging c++ request");
+    }
+
+    class Init {
+    public:
+        Init( struct uwsgi_plugin &plugin ) {
+            plugin.name = "uwsgi_cpp";
+            plugin.modifier1 = 250;
+            plugin.init = uwsgi_cplusplus_init;
+            plugin.request = uwsgi_cplusplus_request;
+            plugin.after_request = uwsgi_cplusplus_after_request;
+        }
+    };
+
+    struct uwsgi_plugin uwsgi_cpp_test_plugin;
+    static Init init( uwsgi_cpp_test_plugin );
 }
--- cpp/uwsgi/test_handler.cpp
+++ cpp/uwsgi/test_handler.cpp
@@ -1,5 +1,6 @@
 #include "base.hpp"
 #include <string>
+#include <iostream>
 
 using namespace uwsgi;
 using namespace std;
@@ -9,13 +10,14 @@ public:
     HelloClass() {
         uwsgi::register_request( bind( &HelloClass::hello_world, this, placeholders::_1 ));
     }
+
     void hello_world(Request &req);
 };
 
 void HelloClass::hello_world(Request &req) {
-    string path_info = req.get("PATH_INFO");
-
     req.prepare_headers("200 OK");
     req.add_content_type("text/html");
-    req.write_body(path_info);
+    req.write_body("Hello, world");
 }
+
+static HelloClass hello;
